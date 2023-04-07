75809f5c496a1da5248f535f7e04c05cee59938f
rahilabdeljalil <55146175+rahilabdeljalil@users.noreply.github.com>
2023-04-07 19:48:25 +0000

    Add files via upload

---
+++ ridex-master/assets/js/script.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/**
+ * navbar toggle
+ */
+
+const overlay = document.querySelector("[data-overlay]");
+const navbar = document.querySelector("[data-navbar]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+const navToggleFunc = function () {
+  navToggleBtn.classList.toggle("active");
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+navToggleBtn.addEventListener("click", navToggleFunc);
+overlay.addEventListener("click", navToggleFunc);
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", navToggleFunc);
+}
+
+
+
+/**
+ * header active on scroll
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 10 ? header.classList.add("active")
+    : header.classList.remove("active");
+});
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
